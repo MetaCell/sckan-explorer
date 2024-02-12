29c2d8b59a10e2881f9e6d5b2f7820b6038eecc2
vidhya-metacell <vidhya@metacell.us>
2024-02-12 20:15:23 +0100

    #esckan-13 Theme and variables

---
+++ src/theme/variables.ts
@@ -0,0 +1,15 @@
+export const vars = {
+  primaryFont: "'Asap', sans-serif",
+  baseBg: '#2F3032',
+  baseWhite: '#FFF',
+  baseContainerBg: '#F5F7FB',
+  primary500: '#2155BA',
+  primary600: '#8300BF',
+  primaryPurple500: '#5E008A',
+  gray800: '#2F3032',
+  gray700: '#4A4C4F',
+  gray600: '#6C707A',
+  gray400: '#9BA2B0',
+  gray100: '#EDEFF2',
+  gray25: '#FCFCFD',
+};
