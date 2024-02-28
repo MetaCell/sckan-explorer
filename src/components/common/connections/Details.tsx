299af3398c8e183faf35d0f92ab2e5383a02081b
salam dalloul <salamdalloul93@gmail.com>
2024-02-28 12:02:35 +0100

    code refactor

--- src/components/common/connections/Details.tsx
+++ src/components/common/connections/Details.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   Typography,
   Button,
@@ -9,9 +10,31 @@ import { vars } from "../../../theme/variables.ts";
 import PopulationDisplay from "./PopulationDisplay.tsx";
 import CommonAccordion from "../Accordion.tsx";
 import CommonChip from "../CommonChip.tsx";
-import {ArrowOutward, HelpCircle} from "../../../icons";
+import { ArrowOutward, HelpCircle } from "../../../icons";
 
-const { gray500, gray700, gray800} = vars
+const { gray500, gray700, gray800} = vars;
+
+const RowStack = ({ label, value, Icon }: {label: string, value: string, Icon?: React.ElementType}) => (
+  <Stack
+    direction="row"
+    alignItems="center"
+    spacing={'.75rem'}
+    sx={{
+      '& .MuiSvgIcon-root': {
+        height: '1rem',
+        width: '1rem',
+        marginLeft: '.5rem'
+      }
+    }}
+  >
+    <Typography variant='subtitle1' width='6rem'>{label}</Typography>
+    <Typography variant='subtitle1' fontWeight={400}>
+      {value}
+      {Icon && <Icon />}
+    </Typography>
+  </Stack>
+);
+
 const Details = () => {
   return (
     <Stack p='.75rem' spacing='1.5rem'>
@@ -36,63 +59,33 @@ const Details = () => {
           Fifth thoracic dorsal root ganglion to Heart right ventricle via White matter of spinal cord
         </Typography>
         <CommonChip label="Sympathetic" variant="outlined" />
-       <CommonAccordion
-         summary="Connection Details"
-         details={
-           <>
-             <Stack spacing={1}>
-               <Stack
-                 direction="row"
-                 alignItems="center"
-                 spacing='.75rem'
-                 sx={{
-                   '& .MuiSvgIcon-root': {
-                     height: '1rem',
-                     width: '1rem'
-                   }
-                 }}
-               >
-                 <Typography variant='subtitle1' width='6rem'>Status</Typography>
-                 <Typography variant='subtitle1' fontWeight={400} display='flex' alignItems='center' gap='.5rem'>
-                   Inferred
-                   <HelpCircle />
-                 </Typography>
-               </Stack>
-               <Stack
-                 direction="row"
-                 alignItems="center"
-                 spacing='.75rem'
-               >
-                 <Typography variant='subtitle1' width='6rem'>Species</Typography>
-                 <Typography variant='subtitle1' fontWeight={400}>Mammal</Typography>
-               </Stack>
-               <Stack
-                 direction="row"
-                 alignItems="center"
-                 spacing='.75rem'
-               >
-                 <Typography variant='subtitle1' width='6rem'>Label</Typography>
-                 <Typography variant='subtitle1' fontWeight={400}>Neuron type aacar 13</Typography>
-               </Stack>
-               <Stack
-                 direction="row"
-                 alignItems="center"
-                 spacing='.75rem'
-               >
-                 <Typography variant='subtitle1' width='6rem'>Provenances</Typography>
-                 <Stack
-                   direction="row"
-                   alignItems="center"
-                   spacing={'.5rem'}
-                 >
-                   <CommonChip label="www.microsoft.com" variant="outlined" className='link' icon={<ArrowOutwardRoundedIcon fontSize='small' />} />
-                   <CommonChip label="google.com" variant="outlined" className='link' icon={<ArrowOutwardRoundedIcon fontSize='small' />} />
-                 </Stack>
-               </Stack>
-             </Stack>
-           </>
-         }
-       />
+        <CommonAccordion
+          summary="Connection Details"
+          details={
+            <>
+              <Stack spacing={1}>
+                <RowStack label="Status" value="Inferred" Icon={HelpCircle} />
+                <RowStack label="Species" value="Mammal" />
+                <RowStack label="Label" value="Neuron type aacar 13" />
+                <Stack
+                  direction="row"
+                  alignItems="center"
+                  spacing='.75rem'
+                >
+                  <Typography variant='subtitle1' width='6rem'>Provenances</Typography>
+                  <Stack
+                    direction="row"
+                    alignItems="center"
+                    spacing={'.5rem'}
+                  >
+                    <CommonChip label="www.microsoft.com" variant="outlined" className='link' icon={<ArrowOutwardRoundedIcon fontSize='small' />} />
+                    <CommonChip label="google.com" variant="outlined" className='link' icon={<ArrowOutwardRoundedIcon fontSize='small' />} />
+                  </Stack>
+                </Stack>
+              </Stack>
+            </>
+          }
+        />
       </Stack>
       <Divider />
       <PopulationDisplay />
