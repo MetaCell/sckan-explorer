3214e8ed43f1353c5be66dddc9b5d43ad2d1624f
salam dalloul <salamdalloul93@gmail.com>
2024-03-28 04:41:42 +0100

    remove background opacity

--- src/components/graphDiagram/widgets/OriginNodeWidget.tsx
+++ src/components/graphDiagram/widgets/OriginNodeWidget.tsx
@@ -41,7 +41,7 @@ export const OriginNodeWidget: React.FC<OriginNodeProps> = ({
                 gap: "0.25rem",
                 borderRadius: "50%",
                 border: "1.253px solid #6C707A",
-                background: "rgba(246, 247, 249, 0.20)",
+                background: "rgba(246, 247, 249)",
               boxShadow:
                 "0px 1px 3px 0px rgba(16, 24, 40, 0.10), 0px 1px 2px 0px rgba(16, 24, 40, 0.06)",
             }}
