927b96a83eb2cb788226c9690d872df2781ecfc1
D. Gopal Krishna <nikhil0223@gmail.com>
2024-05-13 13:06:59 +0530

    ESCKAN-20 - add types for summary

--- src/components/common/Types.ts
+++ src/components/common/Types.ts
@@ -1,3 +1,5 @@
+import { KnowledgeStatement } from "../../models/explorer";
+
 export type OptionDetail = {
   title: string; // What to display as the title/label for the property.
   value: string; // The actual value/content for the property.
@@ -8,3 +10,12 @@ export type Option = {
   group: string;
   content: OptionDetail[];
 }
+
+export type SummaryType = 'summary' | 'detailedSummary' | 'instruction';
+
+export type KSIDAndOriginType = { origin_name: string, ksIds: string[] }[]
+export type ksMapType = Record<string, { ks: KnowledgeStatement, count: number }>;
+export type PhenotypeDetail = {
+  label: string;
+  color: string;
+};
