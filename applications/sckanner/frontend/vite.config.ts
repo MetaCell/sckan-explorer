25ee325dd787fdfe2e93cb0dbd61db2441662aee
afonso pinto <afonsobspinto@gmail.com>
2024-11-21 12:08:29 +0000

    ESCKAN-76 fix: Remove deprecated env variables import

--- applications/sckanner/frontend/vite.config.ts
+++ applications/sckanner/frontend/vite.config.ts
@@ -1,19 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
-const API_URL = process.env.VITE_API_URL || 'http://localhost:8000';
-
+const API_URL = process.env.VITE_API_URL;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: API_URL, 
-        changeOrigin: true,
+    ...(API_URL && {
+      proxy: {
+        '/api': {
+          target: API_URL,
+          changeOrigin: true,
+        },
       },
-    },
+    }),
   },
 });
