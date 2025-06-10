5cbfa21c22c56d47f507f5bd415a4bb85b0f87ab
ddelpiano <dario@metacell.us>
2025-06-10 16:55:54 +0200

    fix lint issue

--- applications/sckanner/frontend/src/components/connections/shared/ButtonDropDown.tsx
+++ applications/sckanner/frontend/src/components/connections/shared/ButtonDropDown.tsx
@@ -57,7 +57,7 @@ export default function MAPSButton({
   };
 
   const handleMenuItemClick = (
-    event: React.MouseEvent<HTMLLIElement, MouseEvent>,
+    _event: React.MouseEvent<HTMLLIElement, MouseEvent>,
     index: number,
   ) => {
     setSelectedIndex(index);
